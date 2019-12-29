6a291729889e8cca37cddbfdcad9194a69f4a0bb
Jakub Kozłowski <kubukoz@gmail.com>
2019-12-29 04:48:08 +0100

    Move logic to a different file

--- src/main/scala/com/kubukoz/next/Main.scala
+++ src/main/scala/com/kubukoz/next/Main.scala
@@ -1,22 +1,10 @@
 package com.kubukoz.next
 
-import com.kubukoz.next.util.middlewares
 import cats.effect.Console.implicits._
 import com.monovore.decline._
 import com.monovore.decline.effect._
 
-import org.http4s.client.blaze.BlazeClientBuilder
-import scala.concurrent.ExecutionContext
-import java.nio.file.Paths
-import java.lang.System
-import org.http4s.client.middleware.FollowRedirect
-import org.http4s.client.middleware.RequestLogger
-import org.http4s.client.middleware.ResponseLogger
-
 import cats.data.NonEmptyList
-import org.http4s.client.Client
-import com.kubukoz.next.util.Config
-import com.olegpy.meow.hierarchy.deriveApplicativeAsk
 import ConfigLoader.deriveAskFromLoader
 
 sealed trait Choice extends Product with Serializable
@@ -38,46 +26,13 @@ object Choice {
 
 object Main extends CommandIOApp(name = "spotify-next", header = "Gather great music") {
 
-  val configPath = Paths.get(System.getProperty("user.home") + "/.spotify-next.json")
+  import Program._
 
-  def makeLoader[F[_]: Sync: ContextShift] =
-    Blocker[F].map(ConfigLoader.default[F](configPath, _)).evalMap(ConfigLoader.cached(_))
-
-  def loginUser[F[_]: Console: ConcurrentEffect: Timer: ConfigLoader]: F[Unit] = {
-    implicit val login: Login[F] = Login.blaze[F]
-
-    for {
-      token <- Login[F].server
-      config <- ConfigLoader[F].loadConfig
-      newConfig = config.copy(token = token)
-      _ <- ConfigLoader[F].saveConfig(newConfig)
-      _ <- Console[F].putStrLn("Saved token to file")
-    } yield ()
-  }
-
-  def makeClient[F[_]: ConcurrentEffect: ContextShift: Timer: Console: ConfigLoader]: Resource[F, Client[F]] =
-    BlazeClientBuilder(ExecutionContext.global)
-      .resource
-      .map(FollowRedirect(maxRedirects = 5))
-      .map(RequestLogger(logHeaders = true, logBody = true))
-      .map(ResponseLogger(logHeaders = true, logBody = true))
-      .map(middlewares.withToken)
-      .map(middlewares.retryUnauthorizedWith(loginUser[F]))
-      .map(middlewares.implicitHost("api.spotify.com"))
-
-  def makeSpotify[F[_]: Console: Sync: Config.Ask](client: Client[F]) = {
-    implicit val theClient = client
-
-    Spotify.instance[F]
-  }
-
-  import Choice._
-
-  def runApp(implicit loader: ConfigLoader[IO]): Choice => Spotify[IO] => IO[Unit] = {
-    case Login          => _ => loginUser[IO]
-    case NextTrack      => _.nextTrack
-    case DropTrack      => _.dropTrack
-    case FastForward(p) => _.fastForward(p)
+  def runApp[F[_]: ConfigLoader: Login: Console: Monad]: Choice => Spotify[F] => F[Unit] = {
+    case Choice.Login          => _ => loginUser[F]
+    case Choice.NextTrack      => _.nextTrack
+    case Choice.DropTrack      => _.dropTrack
+    case Choice.FastForward(p) => _.fastForward(p)
   }
 
   val main: Opts[IO[ExitCode]] =
@@ -85,7 +40,9 @@ object Main extends CommandIOApp(name = "spotify-next", header = "Gather great m
       .opts
       .map { choice =>
         makeLoader[IO].use { implicit loader =>
-          makeClient[IO].map(makeSpotify[IO](_)).use(runApp(loader)(choice))
+          implicit val login: Login[IO] = Login.blaze[IO]
+
+          makeClient[IO].map(makeSpotify[IO](_)).use(runApp[IO].apply(choice))
         }
       }
       .map(_.as(ExitCode.Success))
---
+++ src/main/scala/com/kubukoz/next/Program.scala
@@ -0,0 +1,50 @@
+package com.kubukoz.next
+
+import com.kubukoz.next.util.middlewares
+
+import org.http4s.client.blaze.BlazeClientBuilder
+import scala.concurrent.ExecutionContext
+import java.nio.file.Paths
+import java.lang.System
+import org.http4s.client.middleware.FollowRedirect
+import org.http4s.client.middleware.RequestLogger
+import org.http4s.client.middleware.ResponseLogger
+
+import org.http4s.client.Client
+import com.kubukoz.next.util.Config
+import com.olegpy.meow.hierarchy.deriveApplicativeAsk
+import ConfigLoader.deriveAskFromLoader
+
+object Program {
+  val configPath = Paths.get(System.getProperty("user.home") + "/.spotify-next.json")
+
+  def makeLoader[F[_]: Sync: ContextShift] =
+    Blocker[F].map(ConfigLoader.default[F](configPath, _)).evalMap(ConfigLoader.cached(_))
+
+  def makeClient[F[_]: ConcurrentEffect: ContextShift: Timer: Console: ConfigLoader: Login]: Resource[F, Client[F]] =
+    BlazeClientBuilder(ExecutionContext.global)
+      .resource
+      .map(FollowRedirect(maxRedirects = 5))
+      .map(RequestLogger(logHeaders = true, logBody = true))
+      .map(ResponseLogger(logHeaders = true, logBody = true))
+      .map(middlewares.withToken)
+      .map(middlewares.retryUnauthorizedWith(loginUser[F]))
+      .map(middlewares.implicitHost("api.spotify.com"))
+
+  // Do NOT move this into Spotify, it'll vastly increase the range of its responsibilities!
+  def loginUser[F[_]: Console: Login: ConfigLoader: Monad]: F[Unit] =
+    for {
+      token <- Login[F].server
+      config <- ConfigLoader[F].loadConfig
+      newConfig = config.copy(token = token)
+      _ <- ConfigLoader[F].saveConfig(newConfig)
+      _ <- Console[F].putStrLn("Saved token to file")
+    } yield ()
+
+  def makeSpotify[F[_]: Console: Sync: Config.Ask](client: Client[F]) = {
+    implicit val theClient = client
+
+    Spotify.instance[F]
+  }
+
+}
