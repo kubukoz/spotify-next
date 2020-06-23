9e87f77e5941554b4dbbba03e9b635decb49e3b7
Jakub Kozłowski <kubukoz@gmail.com>
2020-06-23 22:58:14 +0200

    Remove some boilerplate (#59)

--- src/main/scala/com/kubukoz/next/util/traverse.scala
+++ src/main/scala/com/kubukoz/next/util/traverse.scala
@@ -4,22 +4,6 @@ import cats.tagless.InvariantK
 
 object traverse {
 
-  // this should be in cats, it's literally code from the Traverse+Reducible instance
-  implicit def tuple2NonEmptyTraverse[X]: NonEmptyTraverse[(X, *)] = new NonEmptyTraverse[(X, *)] {
-
-    def nonEmptyTraverse[G[_], A, B](fa: (X, A))(f: A => G[B])(implicit G: Apply[G]): G[(X, B)] =
-      G.map(f(fa._2))((fa._1, _))
-
-    def foldLeft[A, B](fa: (X, A), b: B)(f: (B, A) => B): B = f(b, fa._2)
-
-    def foldRight[A, B](fa: (X, A), lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = f(fa._2, lb)
-
-    def reduceLeftTo[A, B](fa: (X, A))(f: A => B)(g: (B, A) => B): B = f(fa._2)
-
-    def reduceRightTo[A, B](fa: (X, A))(f: A => B)(g: (A, Eval[B]) => Eval[B]): Eval[B] =
-      Now(f(fa._2))
-  }
-
   // this should be in cats-tagless
   implicit val invariantKNonEmptyTraverse: InvariantK[NonEmptyTraverse] = new InvariantK[NonEmptyTraverse] {
 
