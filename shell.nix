1971b082e841173dc647ef917cc6b1213db43ad4
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-28 02:33:59 +0100

    Add nodejs to nix

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ let
 
   pkgs = import nixpkgs { };
 in pkgs.mkShell {
-  buildInputs = [ pkgs.netlify-cli ];
+  buildInputs = [ pkgs.netlify-cli pkgs.nodejs-14_x ];
   shellHook = "export NETLIFY_AUTH_TOKEN=${secrets.netlify.token}";
 }
 
