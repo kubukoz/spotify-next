d2d79ee0c39b3180d5af47242595495233ea09ff
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-28 02:25:13 +0100

    Add actual token

--- shell.nix
+++ shell.nix
@@ -3,7 +3,11 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
     sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   };
+  secrets = import ./secrets.nix;
 
   pkgs = import nixpkgs { };
-in pkgs.mkShell { buildInputs = [ pkgs.netlify-cli ]; }
+in pkgs.mkShell {
+  buildInputs = [ pkgs.netlify-cli ];
+  shellHook = "export NETLIFY_AUTH_TOKEN=${secrets.netlify.token}";
+}
 
