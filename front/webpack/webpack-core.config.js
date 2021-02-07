b997817c5cd263d62567ea802e85c8a0ffd68cfb
Jakub Kozłowski <kubukoz@gmail.com>
2021-02-07 04:15:25 +0100

    Remove old frontend, now new frontend is my best friend

---
+++ front/webpack/webpack-core.config.js
@@ -0,0 +1,54 @@
+var path = require("path");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    alias: {
+      "resources": path.resolve(__dirname, "../../../../src/main/resources"),
+      "js": path.resolve(__dirname, "../../../../src/main/js"),
+      "scalajs": path.resolve(__dirname, "./scalajs-entry.js")
+    },
+    modules: [ path.resolve(__dirname, 'node_modules') ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      // "file" loader for svg
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            query: {
+              name: 'static/media/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, "../../../../src/main/public") }
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../../../../src/main/public/index.html")
+    })
+  ],
+  output: {
+    devtoolModuleFilenameTemplate: (f) => {
+      if (f.resourcePath.startsWith("http://") ||
+          f.resourcePath.startsWith("https://") ||
+          f.resourcePath.startsWith("file://")) {
+        return f.resourcePath;
+      } else {
+        return "webpack://" + f.namespace + "/" + f.resourcePath;
+      }
+    }
+  }
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,6 @@
 addSbtPlugin("com.geirsson" % "sbt-ci-release" % "1.5.5")
-addSbtPlugin("ch.epfl.scala" % "sbt-bloop" % "1.4.6")
 addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.16")
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.8.0")
+
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.4.0")
+addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.20.0")
