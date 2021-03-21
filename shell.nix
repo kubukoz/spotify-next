d5200e034d64cc6fe224c2c377d5d6e0e963755e
Jakub Kozłowski <kubukoz@gmail.com>
2021-03-21 22:37:30 +0100

    Fix nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 let
   nixpkgs = import ./pkgs.nix;
-  pkgs = import nixpkgs {};
+  sbt-overlay = self: super: rec {
+    jre = self.jdk11;
+    jdk = self.jdk11;
+    scala = super.scala.override { inherit jre; };
+  };
+  pkgs = import nixpkgs { overlays = [ sbt-overlay ]; };
 in
 pkgs.mkShell { buildInputs = with pkgs;[ sbt nodejs-12_x ]; }
